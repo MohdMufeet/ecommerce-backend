c03b43c5ac371af07541d4df8d22e403192b0c37
MohdMufeet <mohdmufeet786@gmail.com>
2026-06-14 00:33:38 +0530

    fix: Correct token generation and verification logic in JWT utility functions

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,21 +2,22 @@ const jwt = require("jsonwebtoken");
 
 SECRET_KEY = process.env.SECRET_KEY;
 
-const generateToken = (token) => {
+const generateToken = (userId) => {
   try {
-    const genratedToken = jwt.sign(token, SECRET_KEY, { expiresIn: "7d" });
+    const genratedToken = jwt.sign({_id:userId}, SECRET_KEY, { expiresIn: "7d" });
     return genratedToken;
   } catch (err) {
-    next(err);
+    throw new Error(err);
+
   }
 };
 
-const checkToken = (token) => {
+const checkToken = (userId) => {
   try {
-    const verifiedToken = jwt.verify(token, SECRET_KEY);
+    const verifiedToken = jwt.verify({_id:userId}, SECRET_KEY);
     return verifiedToken;
   } catch (err) {
-    next(err);
+    throw new Error(err);
   }
 };
 
