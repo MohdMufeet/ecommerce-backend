af1ca2a87eb7c79dfcd886bc99644793a745f8c9
MohdMufeet <mohdmufeet786@gmail.com>
2026-06-15 23:08:03 +0530

    fix: Add missing category field to product schema

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -16,6 +16,10 @@ const productSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    category: {
+      type: String,
+      required: true,
+    },
     stock: {
       type: Number,
       default: 0,
